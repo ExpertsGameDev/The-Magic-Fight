53b16c2c661df0873e377c8c8bf7102811691afd
AAALLLEEEXXX <aaallleeexxx918@gmail.com>
2020-03-01 19:24:26 +0300

    + slot

---
+++ Assets/Scripts/RandomItem.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomItem : MonoBehaviour
+{
+    public GameObject[] items;
+    
+    void Start()
+    {
+        int i = Random.Range(0, items.Length);
+        Instantiate(items[i], transform.position, Quaternion.identity);
+    }
+}
